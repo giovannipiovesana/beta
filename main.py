e2c6278c90618fce8d1c9aa33eaa03d44b172f2e
Luca Mattioli <57446476+luca-mattioli@users.noreply.github.com>
2019-12-30 17:14:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -36,11 +36,11 @@ def parse_arguments(states, capitals):
 
 def state_capital():
         states, capitals = parse_allowed_input()
-    args = parse_arguments(states, capitals)
-    if args.state:
-        capital_checker = check_capital(args.state)
-    else: 
-        state_checker = check_state(args.capital)
+        args = parse_arguments(states, capitals)
+        if args.state:
+            capital_checker = check_capital(args.state)
+        else:
+            state_checker = check_state(args.capital)
 
 def db_check():
         conn = sqlite3.connect('capitals.sqlite')
