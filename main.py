14e9f50a4bbd22893473da8acdd3d664cc2cdcff
Luca Mattioli <57446476+luca-mattioli@users.noreply.github.com>
2020-01-04 20:53:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,23 @@ import argparse
 import sqlite3
 import sys
 
-# This function parses the user inputs.
-'''def parse_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('city', type=str,
-                        help='The name of the capital')
-    parser.add_argument('been', type=str,
-                        help='been')
-    parser.add_argument('-check', type=str,
-                        help='The name of the capital')
-    args = parser.parse_args()
-    return args'''
+import csv
+from directory.capitals import check_capital
+from directory.capitals import check_state
+
+data ='directory/capitals.csv'
+
+# This function creates two sets from the csv file in order to whitelist user inputs
+def parse_allowed_input(datafile=data):
+    states = set()
+    capitals = set()
+    with open(datafile) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        next(csv_reader)
+        for row in csv_reader:
+            states.add(row[0])
+            capitals.add(row[1])
+    return states, capitals
 
 def db_edit():
     conn = sqlite3.connect('capitals.sqlite')
@@ -33,11 +39,22 @@ def db_check():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--capital', type=str,
+                        help='The name of the state', choices=states)
+    parser.add_argument('--state', type=str,
+                        help='The name of the capital', choices=capitals)
     parser.add_argument('--city', type=str,
                         help='The name of the capital')
     parser.add_argument('--been', type=str,
                         help='been')
     args = parser.parse_args()
     
-    db_edit()
-    db_check()
+    states, capitals = parse_allowed_input()
+    args = parse_arguments(states, capitals)
+    if args.state:
+        capital_checker = check_capital(args.state)
+    if args.capital
+        state_checker = check_state(args.capital)
+    else:
+        db_edit()
+        db_check()
