0c016c54d8b18a3c74d322fdcbb55e65013d5115
Luca Mattioli <57446476+luca-mattioli@users.noreply.github.com>
2020-01-04 23:02:09 +0100

    Add files via upload

---
+++ tests/test_capitals.py
@@ -0,0 +1,38 @@
+#This module runs three different tests to the capitals.py module in both check_capital and check_state
+import unittest
+from Week_2.capitals import check_capital
+from Week_2.capitals import check_state
+import os
+import sys
+
+class TestTrue(unittest.TestCase):
+    def setUp(self):
+        self.list = str('Andorra')
+        self.list_boundary = ('andorra', 'aland island')
+        self.list_empty = ()
+
+#Test 1: test with an invalid type input.   
+    def test_invalid (self):
+        a = check_state(self.list)
+        b = check_capital(self.list)
+        self.assertFalse(a)
+        self.assertFalse(b)
+        
+#test 2: Test with an empty list.
+    def test_empty(self):
+        c = check_state(self.list_empty)
+        d = check_capital(self.list_empty)
+        self.assertFalse(c)
+        self.assertFalse(d)
+
+#test 3: Test with a corner case since lowercase aren't allowed.
+    def test_cornercase(self):
+        e = check_state(self.list_boundary)
+        f = check_capital(self.list_boundary)
+        self.assertFalse(e)
+        self.assertFalse(f)
+ 
+    def tearDown(self):
+        del self.list
+        del self.list_empty
+        del self.list_boundary
