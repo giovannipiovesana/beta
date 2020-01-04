f093f5c2c62f544065280d802e81dc29ce09a908
Luca Mattioli <57446476+luca-mattioli@users.noreply.github.com>
2020-01-05 00:21:48 +0100

    Update capitals.py

--- directory/capitals.py
+++ directory/capitals.py
@@ -64,9 +64,11 @@ def check_capital(state_name):
     '''given the State it gets the capital'''
 
     if state_name in list_of_capitals:
-        print 'The capital of {} is {}'.format(state_name, list_of_capitals[state_name])
+        print ('The capital of {} is {}'.format(state_name,
+                                                list_of_capitals[state_name]))
     else:
-        print 'Sorry, {} does not seem to be an European state'.format(state_name)
+        print ('Sorry, {} does not seem to be an European state'
+               .format(state_name))
 
 
 def check_state(capital_name):
@@ -74,6 +76,8 @@ def check_state(capital_name):
 
     for (state, capital) in list_of_capitals.items():
         if capital == capital_name:
-            print 'The European state whose capital is {} is {}'.format(capital_name, state)
-    return
-    print 'Sorry, {} is not the capital of any European state'.format(capital_name)
+            print ('The European state whose capital is {} is {}'
+                   .format(capital_name, state))
+            return
+    print ('Sorry, {} is not the capital of any European state'
+           .format(capital_name))
