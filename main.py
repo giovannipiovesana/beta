42b5bc67bfb76e1e5e6e6cca99af3fffa12ea789
Luca Mattioli <57446476+luca-mattioli@users.noreply.github.com>
2020-01-04 22:20:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,28 +10,7 @@ from directory.db_manager import db_create
 from directory.db_manager import db_edit
 from directory.db_manager import db_check
 
-'''def db_create():
-    conn = sqlite3.connect('capitals.sqlite')
-    cur = conn.cursor()
-    
-    cur.execute('DROP TABLE IF EXISTS capitals')
-    cur.execute('''
-    CREATE TABLE "capitals"(
-    "capital_id" TEXT,
-    "state_id" TEXT,
-    "note_id" TEXT)''')
-    
-    with open('directory/capitals.csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for row in csv_reader:
-            print(row)
-            capital_id=row[0]
-            state_id=row[1]
-            cur.execute('''INSERT INTO capitals(
-            capital_id, state_id) VALUES (?,?)''',
-                        (capital_id, state_id))
-            conn.commit()
-
+'''
 def db_edit():
     conn = sqlite3.connect('capitals.sqlite')
     cur = conn.cursor()
