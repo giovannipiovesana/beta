9df66d156aff9b771ee32b3a6919c4f58738f596
Luca Mattioli <57446476+luca-mattioli@users.noreply.github.com>
2020-01-05 00:02:16 +0100

    Update test_capitals.py

--- tests/test_capitals.py
+++ tests/test_capitals.py
@@ -5,33 +5,37 @@ from directory.capitals import check_state
 import os
 import sys
 
+
 class TestTrue(unittest.TestCase):
+
     def setUp(self):
         self.list = str('Andorra')
         self.list_boundary = ('andorra', 'aland island')
         self.list_empty = ()
 
+# Test 1: test with an invalid type input.
 
-    def test_invalid (self):
+    def test_invalid(self):
         a = check_state(self.list)
         b = check_capital(self.list)
         self.assertFalse(a)
         self.assertFalse(b)
 
-        
+# test 2: Test with an empty list.
+
     def test_empty(self):
         c = check_state(self.list_empty)
         d = check_capital(self.list_empty)
         self.assertFalse(c)
         self.assertFalse(d)
 
-        
+# test 3: Test with a corner case since lowercase aren't allowed.
+
     def test_cornercase(self):
         e = check_state(self.list_boundary)
         f = check_capital(self.list_boundary)
         self.assertFalse(e)
         self.assertFalse(f)
- 
 
     def tearDown(self):
         del self.list
