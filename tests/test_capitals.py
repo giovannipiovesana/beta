9d9299f4ade056c7187ffb2b2880fa93ae3325c9
Luca Mattioli <57446476+luca-mattioli@users.noreply.github.com>
2020-01-04 23:16:40 +0100

    Update test_capitals.py

--- tests/test_capitals.py
+++ tests/test_capitals.py
@@ -16,8 +16,8 @@ class TestTrue(unittest.TestCase):
     def test_invalid (self):
         a = check_state(self.valid_state)
         b = check_capital(self.valid_capital)
-        self.assertFalse(a)
-        self.assertFalse(b)
+        self.assertTrue(a)
+        self.assertTrue(b)
         
 #test 2: Test with an empty list.
     def test_empty(self):
