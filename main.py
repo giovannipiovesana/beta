7ade58b72072b44bac918136e90ab0c77f0466da
Luca Mattioli <57446476+luca-mattioli@users.noreply.github.com>
2020-01-04 22:25:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
                         help='been')
     args = parser.parse_args()
     
-    if args.create = True:
+    if args.create == True:
         db_create()
     if args.state:
         capital_checker = check_capital(args.state)
