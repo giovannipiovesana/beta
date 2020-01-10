d45ca3250b8a1e38c62d300e62e5e53c38f68c0e
Luca Mattioli <57446476+luca-mattioli@users.noreply.github.com>
2020-01-10 12:16:30 +0100

    Update capitals1.py

--- directory/capitals1.py
+++ directory/capitals1.py
@@ -1,12 +1,21 @@
 import csv
 
 
-list_of_capitals = {}
-with open('directory/capitals.csv') as csvfile:
-    reader = csv.reader(csvfile, delimiter=';')
-    try:
-        for row in reader:
-            list_of_capitals[row[0]] = row[1]
+def load_csv('directory/capitals.csv'):
+
+    '''loads file passed as filename (path of the file) and
+    returns a dictionary. intended for loading csv files'''
+
+    list_of_capitals = {}
+    with open('directory/capitals.csv') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        try:
+            for row in reader:
+                list_of_capitals[row[0]] = row[1]
+        except IndexError:
+            pass
+
+    return list_of_capitals
 
 def check_capital(state_name):
     '''It returns the capital of the correspondent state if it
