8f6b0bbb3c5277a5c80e56b5cba52b8fee6dc630
Luca Mattioli <57446476+luca-mattioli@users.noreply.github.com>
2020-01-10 09:23:18 +0100

    Update capitals1.py

--- directory/capitals1.py
+++ directory/capitals1.py
@@ -1,10 +1,15 @@
-def load_csv(filename):
+def load_csv():
 
     '''loads file passed as filename (path of the file) and
     returns a dictionary. intended for loading csv files'''
 
     list_of_capitals = {}
-    with open(filename) as csvfile:
+    with open('directory/capitals.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            print (row)
+            capital_id = row[0]
+            state_id = row[1]) as csvfile:
         reader = csv.reader(csvfile, delimiter=';')
         try:
             for row in reader:
@@ -13,3 +18,5 @@ def load_csv(filename):
             pass
 
     return list_of_capitals
+
+print (list_of_capitals)
