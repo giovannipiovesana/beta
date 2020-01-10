7a52be059af0b15460a7c3e29153e2b0231eff9b
Luca Mattioli <57446476+luca-mattioli@users.noreply.github.com>
2020-01-10 15:03:09 +0100

    Update capitali.py

--- directory/capitali.py
+++ directory/capitali.py
@@ -7,3 +7,7 @@ def load_csv(filename):
         next(reader)
         list_of_capitals = dict(reader)
     return list_of_capitals
+
+if __name__ == '__main__':
+    list_of_capitals = load_csv(filename)
+    print (list_of_capitals)
